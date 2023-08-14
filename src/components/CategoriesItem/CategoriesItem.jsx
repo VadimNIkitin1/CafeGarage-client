b87a4326cdf2d984d7cfa74aae36dee04b5a0b42
VadimNikitin1 <vchivas18years@gmail.com>
2023-08-14 21:16:00 +0300

    add toggleTabs (no fix)

--- src/components/CategoriesItem/CategoriesItem.jsx
+++ src/components/CategoriesItem/CategoriesItem.jsx
@@ -3,9 +3,17 @@ import "./CategoriesItem.css";
 const CategoriesItem = ({ category }) => {
   const { name } = category;
 
+  const toggleTabs = (e) => {
+    e.target.className = "category-active";
+  };
+
   return (
     <button>
-      <a className={"category"} href={`#${name}`}>
+      <a
+        className={"category"}
+        href={`#${name}`}
+        onClick={(e) => toggleTabs(e)}
+      >
         {name}
       </a>
     </button>
