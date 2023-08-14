c1debac76f00b239ddc6925a277faf7eb968a139
VadimNikitin1 <vchivas18years@gmail.com>
2023-08-14 21:08:05 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import { useTelegram } from "./hooks/useTelegram";
 import "./App.css";
 import CategoriesList from "./components/CategoriesList/CategoriesList";
 import ProductList from "./components/ProductList/ProductList";
+import { useEffect } from "react";
 
 function App() {
-  const { onClose } = useTelegram();
+  const { onClose, tg } = useTelegram();
+
+  useEffect(() => {
+    tg.expand();
+  }, []);
+
   return (
     <div className={"app"}>
       <CategoriesList />
