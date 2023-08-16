85872e54790ffda043874ad296153b3d3c7f7e76
VadimNikitin1 <vchivas18years@gmail.com>
2023-08-16 23:03:47 +0300

    fix

--- src/Pages/ProductPage/ProductPage.jsx
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -2,16 +2,19 @@ import { defer, useAsyncValue, useLoaderData } from "react-router-dom";
 import { getProductById } from "../../services/services";
 
 import style from "./ProductPage.module.css";
+// import { products } from "../../mockDB/index.js";
 
 const ProductPage = () => {
   const { product } = useLoaderData();
-  const { description, imagePath, name, price } = product;
+
+  const { description, imagePath, name } = product;
+
   return (
     <div className={style.productPage}>
       <img
         className={style.img}
         src={`http://localhost:4200/${imagePath}`}
-        alt={imagePath}
+        alt={`${imagePath}`}
       />
       <h1 className={style.name}>{name}</h1>
       <i className={style.description}>{description}</i>
