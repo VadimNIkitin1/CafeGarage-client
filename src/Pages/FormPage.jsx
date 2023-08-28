e479d507cebd2519b3dac7c0170eb56a15721a8d
VadimNikitin1 <vchivas18years@gmail.com>
2023-08-28 22:43:59 +0300

    add backbutton

--- src/Pages/FormPage.jsx
+++ src/Pages/FormPage.jsx
@@ -1,6 +1,17 @@
 import { OrderList } from "../components/OrderList/OrderList";
+import { useTelegram } from "../hooks/useTelegram";
 
 const FormPage = () => {
+  const { tg } = useTelegram();
+
+  tg.BackButton.show();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  tg.onEvent("backButtonClicked", goBack);
+
   return (
     <div>
       <OrderList />
