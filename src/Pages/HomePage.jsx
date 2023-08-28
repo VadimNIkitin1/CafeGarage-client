adf7171de645fe0110cd96b2703c20042b223564
VadimNikitin1 <vchivas18years@gmail.com>
2023-08-28 22:22:06 +0300

    add main button

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,27 @@
+import { useEffect } from "react";
 import { CategoriesList } from "../components/CategoriesList/CategoriesList";
 import ProductList from "../components/ProductList/ProductList";
 import { useTelegram } from "../hooks/useTelegram";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
   const { tg } = useTelegram();
   tg.BackButton.hide();
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: "Перейти в корзину",
+    });
+  }, []);
+
+  useEffect(() => {
+    tg.onEvent("mainButtonClicked", navigate("/form", { replace: true }));
+    return () => {
+      tg.offEvent("mainButtonClicked", navigate("/form", { replace: true }));
+    };
+  }, [navigate]);
+
   tg.MainButton.show();
 
   return (
