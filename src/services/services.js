4e4e57a4d6e8329f91af05b6a5d85a25b0740d7e
VadimNikitin1 <vchivas18years@gmail.com>
2023-09-10 18:29:04 +0300

    а

--- src/services/services.js
+++ src/services/services.js
@@ -43,6 +43,7 @@ export const addToCart = async (id) => {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Headers": "*",
           "Access-Control-Allow-Methods": "*",
+          "Access-Control-Allow-Credentials": true,
         },
       }
     );
