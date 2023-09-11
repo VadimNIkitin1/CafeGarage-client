506a458f62890d38c10390a68daae7580badd14a
VadimNikitin1 <vchivas18years@gmail.com>
2023-09-11 15:50:10 +0300

    fix

--- src/services/services.js
+++ src/services/services.js
@@ -40,13 +40,6 @@ export const addToCart = async (id) => {
       {
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin":
-            "https://delightful-speculoos-c98bd5.netlify.app",
-          "Access-Control-Allow-Headers":
-            "access-control-allow-methods, access-control-allow-headers,access-control-allow-credentials, access-control-allow-origin",
-          "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-          "Access-Control-Allow-Credentials": "true",
-          "Access-Control-Request-Headers": "content-type",
         },
       }
     );
