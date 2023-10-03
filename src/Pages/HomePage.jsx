6a170fa28da2b6dafe692557b3f77eecb6cc9dbd
VadimNikitin1 <vchivas18years@gmail.com>
2023-10-03 11:11:01 +0400

    fix

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, createContext } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import CategoriesList from "../components/CategoriesList/CategoriesList";
