d454066a4dbfc88f6194f17e53d1201db54bdf92
VadimNikitin1 <vchivas18years@gmail.com>
2023-10-10 15:05:03 +0400

    срусл

--- src/Pages/ProductPage/ProductPage.jsx
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -18,11 +18,11 @@ const ProductPage = () => {
   const quantity = useSelector((state) => state.cart.quantity);
   const product = useSelector((state) => state.products.product);
   const { id } = useParams();
-  const { onToggleBackButton, initData2 } = useTelegram();
+  const { onToggleBackButton, initData1 } = useTelegram();
   const { goBack } = useCustomNavigate();
   const { targetProd } = useCart(id);
 
-  console.log(initData2);
+  console.log(initData1);
 
   const { description, name, webp_image_url } = product;
 
